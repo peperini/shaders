5ebfcdedb7d61a1e92c9b38f21e1446db6750591
Jose Felix <jarmando.felix@icloud.com>
2026-03-21 19:19:33 -0700

    update: add 04 shader patterns p

--- 04-shader-patterns/app.js
+++ 04-shader-patterns/app.js
@@ -29,6 +29,47 @@ const fragment = /* glsl */ `
         43758.5453123);
   }
 
+  //	Classic Perlin 2D Noise
+  //	by Stefan Gustavson
+  //
+  vec2 fade(vec2 t) {
+    return t*t*t*(t*(t*6.0-15.0)+10.0);
+  }
+
+  vec4 permute(vec4 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+
+  float cnoise(vec2 P) {
+    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+    vec4 ix = Pi.xzxz;
+    vec4 iy = Pi.yyww;
+    vec4 fx = Pf.xzxz;
+    vec4 fy = Pf.yyww;
+    vec4 i = permute(permute(ix) + iy);
+    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+    vec4 gy = abs(gx) - 0.5;
+    vec4 tx = floor(gx + 0.5);
+    gx = gx - tx;
+    vec2 g00 = vec2(gx.x,gy.x);
+    vec2 g10 = vec2(gx.y,gy.y);
+    vec2 g01 = vec2(gx.z,gy.z);
+    vec2 g11 = vec2(gx.w,gy.w);
+    vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 *= norm.x;
+    g01 *= norm.y;
+    g10 *= norm.z;
+    g11 *= norm.w;
+    float n00 = dot(g00, vec2(fx.x, fy.x));
+    float n10 = dot(g10, vec2(fx.y, fy.y));
+    float n01 = dot(g01, vec2(fx.z, fy.z));
+    float n11 = dot(g11, vec2(fx.w, fy.w));
+    vec2 fade_xy = fade(Pf.xy);
+    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
+  }
+
   vec2 rotate(vec2 uv, float rotation, vec2 mid)
   {
     return vec2(
@@ -63,7 +104,6 @@ const fragment = /* glsl */ `
     /* float strength = mod(vUv.y * 10.0, 1.0);
     strength = step(0.5, strength);*/
 
-
     // ———— Pattern 9 ———————————————————————————————————————————————————————————————————————————
     // Bars on y axis (bigger step limit)
     /* float strength = mod(vUv.y * 10.0, 1.0);
@@ -77,7 +117,7 @@ const fragment = /* glsl */ `
     // ———— Pattern 11  ———————————————————————————————————————————————————————————————————————————
     // Grid (combining them to get kind of a grid)
     /* float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
-    strength += step(0.8, mod(vUv.y * 10.0, 1.0));*/
+    strength += step(0.8, mod(vUv.y * 10.0, 1.0)); */
 
     // ———— Pattern 12  ———————————————————————————————————————————————————————————————————————————
     // Dots (Instead of adding, multiplying)
@@ -202,9 +242,69 @@ const fragment = /* glsl */ `
     float strength = 1.0 -  step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25)); */
 
     // ———— Pattern 39 ———————————————————————————————————————————————————————————————————————————
-    vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 100.0) * 0.1, vUv.y + sin(vUv.x * 100.0) * 0.1);
-    float strength = 1.0 -  step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    /* vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 100.0) * 0.1, vUv.y + sin(vUv.x * 100.0) * 0.1);
+    float strength = 1.0 -  step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25)); */
+
+    // ———— Pattern 40 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x, vUv.y);
+    float strength = angle; */
+
+    // ———— Pattern 41 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    float strength = angle; */
+
+    // ———— Pattern 42 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    angle /= PI * 2.0;
+    angle += 0.5;
+    float strength = angle; */
+
+    // ———— Pattern 43 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    angle /= PI * 2.0;
+    angle += 0.5;
+    angle *= 20.0;
+    angle = mod(angle, 1.0);
+    float strength = angle; */
+
+    // ———— Pattern 44 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    angle /= PI * 2.0;
+    angle += 0.5;
+    float strength = sin(angle * 100.0); */
+
+    // ———— Pattern 45 ———————————————————————————————————————————————————————————————————————————
+    /* float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    angle /= PI * 2.0;
+    angle += 0.5;
+    float sinusoid = sin(angle * 100.0);
+
+    float radius = 0.25 + sinusoid * 0.02;
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius)); */
+
+    // ———— Pattern 46 ———————————————————————————————————————————————————————————————————————————
+    /* float strength = cnoise(vUv * 10.0); */
+
+    // ———— Pattern 47 ———————————————————————————————————————————————————————————————————————————
+    /* float strength = step(0.0, cnoise(vUv * 10.0)); */
+
+    // ———— Pattern 48 ———————————————————————————————————————————————————————————————————————————
+    /* float strength = 1.0 - abs(cnoise(vUv * 10.0)); */
+
+    // ———— Pattern 49 ———————————————————————————————————————————————————————————————————————————
+    /* float strength = sin(cnoise(vUv * 10.0) * 20.0); */
+
+    // ———— Pattern 50 ———————————————————————————————————————————————————————————————————————————
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+
+    // Strength clamped
+    strength = clamp(strength, 0.0, 1.0);
+
+    vec3 blackCol = vec3(0.0);
+    vec3 uvCol = vec3(vUv, 1.0);
+    vec3 mixCol = mix(blackCol, uvCol, strength);
+
+    gl_FragColor = vec4(mixCol, 1.0);
   }
 `
 
@@ -252,4 +352,3 @@ const update = (t) => {
   renderer.render({ scene, camera })
 }
 requestAnimationFrame(update)
-
