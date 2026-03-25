f06fdc045f46df7f5fbad9294f2d5f06a05d676d
Jose Felix <jarmando.felix@icloud.com>
2026-03-25 14:27:14 -0700

    webgl: animated galaxy

--- 07-animated-galaxy/app.js
+++ 07-animated-galaxy/app.js
@@ -8,15 +8,40 @@ const vertex = /* glsl */ `
   uniform mat4 viewMatrix;
   uniform mat4 projectionMatrix;
 
+  uniform float uTime;
   uniform float uSize;
 
   attribute vec3 position;
+  attribute vec3 color;
+  attribute float aScales;
+  attribute vec3 aRandomness;
+
+  varying vec3 vColor;
 
   void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distToCenter) * uTime * 0.2;
+    angle += angleOffset;
+
+    modelPosition.x = cos(angle) * distToCenter;
+    modelPosition.z = sin(angle) * distToCenter;
+
+    // Randomness
+    modelPosition.xzy += aRandomness;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+    gl_Position = projectionPosition;
 
     // gl_PointSize only applicable for gl.POINTS draw mode
-    gl_PointSize = uSize;
+    gl_PointSize = uSize * aScales;
+    gl_PointSize *= ( 1.0 / - viewPosition.z);
+
+    vColor = color;
   }
 `
 
@@ -24,8 +49,27 @@ const vertex = /* glsl */ `
 const fragment = /* glsl */ `
   precision mediump float;
 
+  varying vec3 vColor;
+
   void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // Disc
+    /* float str = distance(gl_PointCoord, vec2(0.5));
+    str = 1.0 - step(0.5, str); */
+
+    // Difusse point
+   /*  float str = distance(gl_PointCoord, vec2(0.5));
+    str *= 2.0;
+    str = 1.0 - str; */
+
+    // Point light
+    float str = distance(gl_PointCoord, vec2(0.5));
+    str = 1.0 - str;
+    str = pow(str, 10.0);
+
+    // Final color
+    vec3 color = mix(vec3(0.0), vColor, str);
+
+    gl_FragColor = vec4(color, 1.0);
   }
 `
 
@@ -33,13 +77,17 @@ const fragment = /* glsl */ `
 const gui = new lil.GUI()
 
 // ———— Renderer ———————————————————————————————————————————————————————————————————————————
-const renderer = new Renderer()
+const renderer = new Renderer({
+  dpr: Math.min(window.devicePixelRatio, 2)
+})
 const gl = renderer.gl
+gl.enable(gl.BLEND)
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE)
 document.body.appendChild(gl.canvas)
 
 // ———— Camera ———————————————————————————————————————————————————————————————————————————
 const camera = new Camera(gl)
-camera.position.set(0, 5, 15)
+camera.position.set(0, 3, 10)
 
 // ———— Orbit ———————————————————————————————————————————————————————————————————————————
 const controls = new Orbit(camera, {
@@ -75,7 +123,6 @@ class GalaxyGenerator {
     this.scene = scene
 
     this.geometry = this.getGeometry()
-    console.log(this.geometry)
     this.program = this.getProgram()
     this.galaxy = this.getGalaxy()
   }
@@ -83,6 +130,8 @@ class GalaxyGenerator {
   getGeometry () {
     const positions = new Float32Array(this.params.count * 3)
     const colors = new Float32Array(this.params.count * 3)
+    const scales = new Float32Array(this.params.count)
+    const randomness = new Float32Array(this.params.count * 3)
 
     const colorInside = new Color(this.params.insideColor)
     const colorOutside = new Color(this.params.outsideColor)
@@ -94,27 +143,38 @@ class GalaxyGenerator {
       const radius = Math.random() * this.params.radius
       const branchAngle = (i % this.params.branches) / this.params.branches * Math.PI * 2
 
+      positions[i3    ] = Math.cos(branchAngle) * radius
+      positions[i3 + 1] = 0.0
+      positions[i3 + 2] = Math.sin(branchAngle) * radius
+
+      // Randomness
+
       const randomX = Math.pow(Math.random(), this.params.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * this.params.randomness
       const randomY = Math.pow(Math.random(), this.params.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * this.params.randomness
       const randomZ = Math.pow(Math.random(), this.params.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * this.params.randomness
 
-      positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-      positions[i3 + 1] = randomY
-      positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
+      randomness[i3    ] = randomX
+      randomness[i3 + 1] = randomY
+      randomness[i3 + 2] = randomZ
 
       // Colors
-      /* const mixedColor = colorInside.copy(colorInside) */
-      const mixedColor = lerp(colorInside, colorOutside, radius / this.params.radius)
+      const t = radius / this.params.radius
 
-      colors[i3    ] = mixedColor.r
-      colors[i3 + 1] = mixedColor.g
-      colors[i3 + 2] = mixedColor.b
+      colors[i3    ] = colorInside.r + (colorOutside.r - colorInside.r) * t;
+      colors[i3 + 1] = colorInside.g + (colorOutside.g - colorInside.g) * t;
+      colors[i3 + 2] = colorInside.b + (colorOutside.b - colorInside.b) * t;
+
+      // Scales
+      scales[i] = Math.random()
     }
 
     const geometry = new Geometry(gl, {
       position: { size: 3, data: positions },
       color: { size: 3, data: colors },
+      aScales: { size: 1, data: scales },
+      aRandomness: { size: 3, data: randomness}
     })
+
     return geometry
   }
 
@@ -123,14 +183,12 @@ class GalaxyGenerator {
       vertex,
       fragment,
       uniforms: {
-        uSize: { value: 8 }
+        uTime: { value: 0 },
+        uSize: { value: 30 * renderer.dpr }
       },
-      depthWrite: false,
-      vertexColors: true
+      transparent: true,
+      depthWrite: false
     })
-    /* program.depthWrite = false
-    program.blending = THREE.AdditiveBlending
-    program.vertexColors = true */
     return program
   }
 
@@ -188,11 +246,14 @@ gui.addColor(params, 'outsideColor').onFinishChange(() => {
 const update = (t) => {
   // Call update again on the next frame
   requestAnimationFrame(update)
-  const elapsedTime = t
+  const elapsedTime = t * 0.001
+
+  proxima.program.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update()
   // Render
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE)
   renderer.render({ scene, camera })
 }
 requestAnimationFrame(update)
